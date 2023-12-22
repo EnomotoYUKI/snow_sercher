86eeb535b7615fe94c801b0ac8757b29bb2c6868
AzureServer <johndoe@example.com>
2023-12-22 02:59:33 +0000

    これからコミットします

---
+++ Azure/data_receaved.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/submit_data', methods=['POST'])
+def receive_data():
+    data = request.json
+    temperature = data.get('temperature')
+    humidity = data.get('humidity')
+    snow_depth = data.get('snow_depth')  # True または False
+
+    # ここでデータを処理（例：データベースへの保存、アラートのチェックなど）
+    print(f"受け取ったデータ: 温度={temperature}, 湿度={humidity}, 積雪量={snow_depth}")
+
+    return jsonify({"status": "success"})
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
--- README.md
+++
@@ -1,2 +0,0 @@
-# snow_sercher
-IPUT 2023後期 IoTサービスデザイン実習内容
